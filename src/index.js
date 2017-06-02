0e7e8f72b37231a388188537292e9d796a0d8208
David Gomes <davidrafagomes@gmail.com>
2017-06-03 00:35:11 +0100

    Small styling changes.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import GamePage from "./view/game-page";
 import { Provider } from "react-redux";
 import store from "./data/store";
 
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 ReactDOM.render((
     <Provider store={store}>
         <HashRouter>
--- src/view/home-page.js
+++ src/view/home-page.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createRoom } from "../actions";
 
+import "./home-page.css";
+
 class HomePage extends React.Component {
     handleClickCreate = () => {
         this.props.dispatch(createRoom());
@@ -10,9 +12,20 @@ class HomePage extends React.Component {
 
     render() {
         return (
-            <div>
-                <Link onClick={this.handleClickCreate} to="/game">Create</Link>
-                <Link to="/game">Join</Link>
+            <div className="home-page">
+                <ul>
+                    <li>
+                        <Link
+                            onClick={this.handleClickCreate}
+                            to="/game"
+                        >
+                            Create
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/game">Join</Link>
+                    </li>
+                </ul>
             </div>
         );
     }
