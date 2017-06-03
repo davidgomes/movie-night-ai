a35f004b1908bc66e141b79a00088a4d5f33f2bc
michel94 <miguel.duarte20@hotmail.com>
2017-06-03 05:36:46 +0100

    Merge. Less rounds

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ ml = Ml()
 app = Flask(__name__)
 CORS(app)
 
-ROUNDS = 8
+ROUNDS = 4
 
 class User:
     def __init__(self, uid, cur_movie):
@@ -22,7 +22,6 @@ class User:
         self.cur_movie = cur_movie
         self.votes = []
 
-    
 
 class Pool:
     def __init__(self):
@@ -43,7 +42,7 @@ class Pool:
             print("Can move to next")
             return 1
         return 0
-        
+
     def sum_votes(self, user):
         votes = np.array([0] * len(user.votes))
         for u in self.users.values():
@@ -70,7 +69,8 @@ class Pool:
                 movie = ml.movie_list[self.movies[user.cur_movie]]
                 return (0, movie)
             elif check == 2:
-                return (2, None)
+                votes = [(j, i) for i,j in self.sum_votes(user)]
+                return (2, votes)
             else:
                 return (1, None)
         else:
